02c68b8919aaa6ac7b539909abec09809212cf0f
Dilshan <malindadilshan1000@gmail.com>
2025-06-29 12:34:47 +0530

    Implemented getProducts from productAPI and Filtering based on category

--- src/app/product-container/product-container.component.ts
+++ src/app/product-container/product-container.component.ts
@@ -3,47 +3,53 @@ import { ProductComponent } from "../product/product.component";
 import { Product } from '../modals/product';
 import { CommonModule } from '@angular/common';
 import { CartService } from '../services/cart.service';
+import { ProductService } from '../services/product.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-container',
-  imports: [ProductComponent,CommonModule],
+  imports: [ProductComponent,CommonModule, FormsModule],
   templateUrl: './product-container.component.html',
   styleUrl: './product-container.component.scss'
 })
 export class ProductContainerComponent {
   
-  constructor(public cartService:CartService) {}
+  constructor(public cartService:CartService , private productService:ProductService) {}
 
 
-  products: Product[] = [
-    {
-      productName: 'Vanilla Cake',
-      price: 100,
-      stockCount: 0,
-      description: 'A delicious vanilla cake with a rich and creamy frosting. Perfect for any occasion.',
-      imgUrl: 'https://recipesblob.oetker.ca/assets/1936ef0980bf466189b285b65ee574e9/360x400/vanilla-cake-one-slice-missing-11.webp'
-    }
-    ,
-    {
-      productName: 'Cheese Cake',
-      price: 1000,
-      stockCount: 10,
-      description: 'A delicious Cheese cake',
-      imgUrl: 'https://cakesbymk.com/wp-content/uploads/2023/11/Template-Size-for-Blog-Photos-24.jpg'
-    },
-
-    {
-      productName: 'lava Cake',
-      price: 400,
-      stockCount: 12,
-      description: 'A delicious Lava cake',
-      imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjUsVwms-rKj51euHVj_Pp_JFtKMiZmkCatg&s'
-    }
-    
-  ];
+  products: Product[] = [];
+  categories: string[] = [];
+  selectedCategory: string = 'all';
+  filteredProducts: Product[] = [];
 
   productOutPutEvent(product: Product) {
     this.cartService.addToCart(product);
   }
+
+  ngOnInit() {
+      this.productService.getProducts().subscribe({
+        next: (data) => {
+
+          this.categories = Array.from(new Set(data.map(product => product.category))) 
+          this.products = data;
+          this.filteredProducts = data
+        },
+        error: (err) => {
+          alert("Request Failed " + err);
+        }
+      })
+  }
+
+    filterByCategory(category: string) {
+      this.selectedCategory = category;
+      
+      if (category == 'all') {
+        this.filteredProducts = this.products
+      } else {
+        this.filteredProducts = this.products.filter(product => product.category === category);
+      }
+
+  }
+
 
 }
